7f99d02e05d47a9dc45ebc15dfe77df5a7e1da4a
Mateo Lopez <lpzmateo@gmail.com>
2021-03-27 19:49:28 +0100

    Basic player control

---
+++ Assets/Player/CharacterInputs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInputs : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float speed;
+
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        if(Input.GetAxisRaw("Jump") == 1)
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * speed * Time.deltaTime, false, jump);
+    }
+}
